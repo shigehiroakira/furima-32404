bacb367fa2799423943b8f1bb2e8fae3d32d6d6a
shigehiroakira <shigehiroakira69@gmail.com>
2020-12-14 15:32:05 +0900

    出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,26 @@
 class ItemsController < ApplicationController
-  def index
-   
+
+
+    def index
+      @item = Item.order("created_at DESC")
   end
-  
+  def new
+  @item =Item.new
+  end
+
+  def create
+    @article = Item.new(item_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:price,:description,:category_id,:status_id,:shipping_cost_id,:shipping_day_id,:prefecture_id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,27 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :description
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :status_id, numericality: { other_than: 1 } 
+    validates :shipping_cost_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :shipping_day_id, numericality: { other_than: 1 } 
+  end
+
+
   belongs_to :user
   has_one_attached :image
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_cost
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_day
 end
