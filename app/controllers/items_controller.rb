62d4bf2a6fb91f20ce32b97e6b6912d9070b1dbf
shigehiroakira <shigehiroakira69@gmail.com>
2020-12-16 16:37:24 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,14 @@ before_action :set_item, only: [:show,:edit, :update]
      end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+     if item.destroy
+      redirect_to root_path
+     end
+     
+  end
+
   private
 
   def item_params
