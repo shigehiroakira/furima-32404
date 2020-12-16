4bdf3933efaf485367fd3b6b9bf1dff572da9b4d
shigehiroakira <shigehiroakira69@gmail.com>
2020-12-16 18:06:29 +0900

    rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,17 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user! , only: [:new,:create,:edit]
-before_action :set_item, only: [:show,:edit, :update,:destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
-    @item =Item.new
+    @item = Item.new
   end
 
   def create
-    
     @item = Item.new(item_params)
-    
+
     if @item.save
       redirect_to root_path
     else
@@ -22,38 +21,35 @@ before_action :set_item, only: [:show,:edit, :update,:destroy]
 
   def show
   end
-  
+
   def edit
-     unless current_user.id == @item.user_id
-      redirect_to root_path
-     end
-    
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-     if @item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
-     else
+    else
       render :edit
-     end
+    end
   end
 
   def destroy
-  if current_user.id == @item.user_id
-    @item.destroy
-     redirect_to root_path
-  else
-    render :index
-     end
-     
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image,:name,:price,:description,:category_id,:status_id,:shipping_cost_id,:shipping_day_id,:prefecture_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :price, :description, :category_id, :status_id, :shipping_cost_id, :shipping_day_id,
+                                 :prefecture_id).merge(user_id: current_user.id)
   end
-  
+
   def set_item
     @item = Item.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,27 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many   :items
-         
-     with_options presence: true do
-      validates :nickname
-      validates :birthday 
+  has_many :items
 
-     end
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな・カタカナ・漢字で入力してください' } do
+    validates :family_name
+    validates :first_name
+  end
 
-      with_options presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな・カタカナ・漢字で入力してください' } do
-        validates :family_name
-        validates :first_name
-      end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
 
-      with_options presence: true, format: { with:/\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' } do
-        validates :family_name_kana
-        validates :first_name_kana
-      end
-
-      with_options presence: true, format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} do
-      
-      validates :password
-      end
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
+    validates :password
+  end
 end
