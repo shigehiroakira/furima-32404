2811a4b7405869c316c4ce437289debea180c575
shigehiroakira <shigehiroakira69@gmail.com>
2020-12-16 18:05:25 +0900

    rubocopを実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,34 +1,30 @@
 class Item < ApplicationRecord
-
   with_options presence: true do
     validates :name
     validates :description
     validates :image
 
-    with_options presence: true, format: {with:/\A([1-9]\d*,)*[1-9]\d*\z/} do
-     validates :price
+    with_options presence: true, format: { with: /\A([1-9]\d*,)*[1-9]\d*\z/ } do
+      validates :price
     end
-     validates :price, inclusion: { in: 300..99999999 }
-    
-     with_options numericality: { other_than: 0} do
-    
+    validates :price, inclusion: { in: 300..99_999_999 }
+
+    with_options numericality: { other_than: 0 } do
       validates :category_id
-      validates :status_id 
+      validates :status_id
       validates :shipping_cost_id
       validates :prefecture_id
       validates :shipping_day_id
-     end
-
+    end
   end
 
   belongs_to :user
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :Prefecture
   belongs_to :shipping_cost
   belongs_to :shipping_day
-
 end
