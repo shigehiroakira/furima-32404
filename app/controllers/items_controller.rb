e0e74d226607369b543b83f9f9fc2d2b86f27b32
shigehiroakira <shigehiroakira69@gmail.com>
2020-12-16 12:31:07 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,19 @@ before_action :authenticate_user! , only: [:new,:create]
     @item = Item.find(params[:id])
   end
   
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+     if item.update(item_params)
+      redirect_to root_path
+     else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
